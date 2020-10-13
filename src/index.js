c0b8847fbee734b238261f5ed757da558176d21e
James Gale <james@Jamess-MacBook-Air.local>
2020-10-13 14:03:29 -0400

    changed alert width

--- src/index.js
+++ src/index.js
@@ -1,26 +1,9 @@
-// import React from "react";
-// import ReactDOM from "react-dom";
-// import { render } from "react-dom";
-// import { transitions, positions, Provider as AlertProvider } from "react-alert";
-// import AlertTemplate from "react-alert-template-basic";
-
-// import { App } from "./components";
-
-// const Root = () => {
-//   <AlertProvider template={AlertTemplate}>
-//     <App />
-//   </AlertProvider>;
-// };
-
-// ReactDOM.render(<Root />, document.getElementById("root"));
-
 import React from "react";
 import { render } from "react-dom";
 import { transitions, positions, Provider as AlertProvider } from "react-alert";
 import AlertTemplate from "react-alert-template-basic";
 import { App } from "./components";
 
-// optional configuration
 const options = {
   timeout: 5000,
   offset: "30px",
